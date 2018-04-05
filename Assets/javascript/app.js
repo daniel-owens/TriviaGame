001a8f57058c357f6a22ce676d593db2ef94ed4d
Daniel Owens <DanielOwens@Daniel-Owenss-MacBook-Pro-2.local>
2018-04-05 15:23:29 -0700

    updates

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,35 +1,128 @@
 
 
+var count = 60;
+var correctCount = 0;
+var wrongCount = 0;
+var unansweredCount = 0;
 
-$(document).ready(function() {
-    $("#start-button").click(function() {
-	var counter = 60;
+$(document).ready(function(){
+
+    $("#quiz").hide();
     
-    setInterval (function() {
-    counter--;
+    window.scrollTo(0, 500);
+
+    $("#start-button").on("click", function(){
+     
+        $("#quiz").show();
+
+        startCountdown();
+        return;
+
+    });
+
+    function countdown(){
       
-        if (counter >= 0) {
-         span = document.getElementById("show-number");
-         span.innerHTML = counter;
+        count--;
+
+        $('#show-number').html(count + " Seconds");
+
+            $("#submit").on("click", function(){
+
+            clearInterval(count)
+
+            });
+
+            if (count === 0){
+              
+                alert("Time's up! Whomp whomp!3");
+
+                $("#quiz").hide();
+
+            }
+
+    }
+
+    function startCountdown(){
+
+        setInterval(countdown, 1000);
+
+    }
+
+    function timeUp(){
+
+        var Q1 = $('input:radio[name="q1"]:checked').val();
+        var Q2 = $('input:radio[name="q2"]:checked').val();
+        var Q3 = $('input:radio[name="q3"]:checked').val();
+        var Q4 = $('input:radio[name="q4"]:checked').val();
+        var Q5 = $('input:radio[name="q5"]:checked').val();
+       
+
+        if(Q1 === undefined){
+            unansweredCount++;
         }
-        if (counter === 0) {
-         alert('Awww bummer, out of time. Whomp whomp.');
-         clearInterval(counter);
-        }}, 1000);
-	
-	});
+        else if(Q1 === "#correct-answer"){
+            correctCount++;
+        }
+        else{
+            wrongCount++;
+        }
+
+
+        if(Q2 == undefined){
+            unansweredCount++;
+        }
+        else if(Q2 == "#correct-answer"){
+            correctCount++;
+        }
+        else{
+            wrongCount++;
+        }
+
+
+        if(Q3 == undefined){
+            unansweredCount++;
+        }
+        else if(Q3 == "#correct-answer"){
+            correctCount++;
+        }
+        else{
+            wrongCount++;
+        }
+
+
+        if(Q4 == undefined){
+            unansweredCount++;
+        }
+        else if(Q4 == "#correct-answer"){
+            correctCount++;
+        }
+        else{
+            wrongCount++;
+        }
+
+
+        if(Q5 == undefined){
+            unansweredCount++;
+        }
+        else if(Q5 == "#correct-answer"){
+            correctCount++;
+        }
+        else{
+            wrongCount++;
+        }
+
+
+        $('#correct-answers').html(correctCount);
+        $('#wrong-answers').html(wrongCount);
+        $('#unanswered').html(unansweredCount);
+       
+       
+
+    }
+
 });
 
-var quizContainer = document.getElementById('quiz');
-var submitButton = document.getElementById('submit');
-var resultsContainer = document.getElementById('results');
-var myQuestions = document.getElementsByClassNames('questions')
 
 
 
-// to do: 
-// identify wrong and right answer for each question
-// have the timer stop at the end when "submit answers" button is clicked
-// have javascript identify all right and wrong answers after submit answers is clicked
-// display final score at the end
-// the reset button needs to start everything back to the beginning
+
